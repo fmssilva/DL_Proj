bc4f391f7fdfcc6d4cbdd0fa2d1ff06b57b23eef
Rúben <raro.silva@campus.fct.unl.pt>
2026-03-13 21:01:41 +0000

    Add drop/no-dropout simpleCNN

--- assignment_1/src/models/cnn.py
+++ assignment_1/src/models/cnn.py
@@ -5,10 +5,11 @@ import torch.nn.functional as F
 from ..config import NUM_CLASSES
 
 class LeNet5(nn.Module):
-    def __init__(self, in_channels: int = 3):
+    def __init__(self, in_channels: int = 3, dropout: float = 0.4):
         super(LeNet5, self).__init__()
 
         fc1_in_size = 16*14*14 if in_channels == 3 else 16 * 5 * 5
+        self.dropout = nn.Dropout(dropout)
         # First convolutional layer: 1 or 3 input channels, 6 output channels, 5x5 kernel
         self.conv1 = nn.Conv2d(in_channels, 6, kernel_size=5, padding=2)
         # First pooling layer
@@ -28,7 +29,9 @@ class LeNet5(nn.Module):
         x = self.pool2(F.relu(self.conv2(x)))  # Conv -> ReLU -> Pooling
         x = x.view(x.size(0), -1)   # Flatten
         x = F.relu(self.fc1(x))     # First fully connected layer
+        x = self.dropout(x)
         x = F.relu(self.fc2(x))     # Second fully connected layer
+        x = self.dropout(x)
         x = self.fc3(x)             # Output layer
 
         return x
